3d8d097599c83692109db6c0c29da50a29b930e4
Stéphane Nicolas <steff.nicolas@gmail.com>
2015-04-01 20:04:58 -0700

    Better process management in java 5

--- ormgap-plugin/src/main/java/com/github/stephanenicolas/ormgap/CreateOrmLiteConfigAction.java
+++ ormgap-plugin/src/main/java/com/github/stephanenicolas/ormgap/CreateOrmLiteConfigAction.java
@@ -4,6 +4,7 @@ import com.j256.ormlite.android.apptools.OrmLiteConfigUtil;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
 /**
@@ -31,26 +32,27 @@ public class CreateOrmLiteConfigAction {
     }
 
     public void execute() throws IOException, InterruptedException {
-        String cmd = "cd "
-            + searchDir.getAbsolutePath()
-            + " && java -cp "
-            + classpath
-            + " com.j256.ormlite.android.apptools.OrmLiteConfigUtil "
-            + configFile;
-
-        System.out.println("Command: " + cmd);
-        Process process = Runtime.getRuntime().exec(cmd);
+        ProcessBuilder builder = new ProcessBuilder("java", "-cp", classpath, "com.j256.ormlite.android.apptools.OrmLiteConfigUtil", configFile);
+        builder.inheritIO();
+        builder.directory(searchDir);
+        Process process = builder.start();
         final int result = process.waitFor();
-        if (result != 0) {
-            final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-            StringBuffer buffer = new StringBuffer();
-            String line;
-            do {
-                line = bufferedReader.readLine();
-                if (line != null) {
-                    buffer.append(line + '\n');
-                }
-            } while (line != null);
-        }
+        StringBuffer buffer = new StringBuffer();
+        readAll(buffer, process.getInputStream());
+        System.out.println("OrmLiteConfigUtil finished with code: " + result);
+        System.out.println(buffer.toString());
+        buffer = new StringBuffer();
+        System.out.println(buffer.toString());
+    }
+
+    private void readAll(StringBuffer buffer, InputStream stream) throws IOException {
+        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream));
+        String line;
+        do {
+            line = bufferedReader.readLine();
+            if (line != null) {
+                buffer.append(line + '\n');
+            }
+        } while (line != null);
     }
 }
